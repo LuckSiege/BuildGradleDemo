4fe8f985559a93d08a2ac569f40cbc6d39e69747
罗孟伟 <luomengwei@laoyuegou.com>
2018-03-13 18:01:42 +0800

    Initial commit.

---
+++ app/src/main/java/com/laoyuegou/gradle/sample/Hello.java
@@ -0,0 +1,10 @@
+package com.laoyuegou.gradle.sample;
+
+/**
+ * @author：luck
+ * @data：2018/3/13 下午6:00
+ * @描述:
+ */
+
+public class Hello {
+}
--- app/src/main/java/com/laoyuegou/gradle/sample/MainActivity.java
+++ app/src/main/java/com/laoyuegou/gradle/sample/MainActivity.java
@@ -3,11 +3,16 @@ package com.laoyuegou.gradle.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author luck
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         setContentView(R.layout.activity_main);
     }
